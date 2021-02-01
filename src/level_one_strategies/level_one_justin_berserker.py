e177f95521be034f10dd7ef897212e279d8c8996
geomeza <george.meza.github@gmail.com>
2021-02-01 17:50:53 +0000

    Added justin_berserk and colby strategies as well as their tests

---
+++ src/level_one_strategies/level_one_justin_berserker.py
@@ -0,0 +1,40 @@
+class LevelOneBerserkerStrategy:
+    # Sends all of its units directly towards the enemy home colony
+
+    def __init__(self, player_num):
+        self.player_index = player_num
+        self.name = 'justin'
+
+    def decide_ship_movement(self, unit_index, hidden_game_state):
+        myself = hidden_game_state['players'][self.player_index]
+        opponent_index = 1 - self.player_index
+        opponent = hidden_game_state['players'][opponent_index]
+
+        unit = myself['units'][unit_index]
+        x_unit, y_unit = unit['coords']
+        x_opp, y_opp = opponent['home_coords']
+
+        translations = [(0,0), (1,0), (-1,0), (0,1), (0,-1)]
+        best_translation = (0,0)
+        smallest_distance_to_opponent = 999999999999
+        for translation in translations:
+            delta_x, delta_y = translation
+            x = x_unit + delta_x
+            y = x_unit + delta_y
+            dist = abs(x - x_opp) + abs(y - y_opp)
+            if dist < smallest_distance_to_opponent:
+                best_translation = translation
+                smallest_distance_to_opponent = dist
+
+        return best_translation
+
+    def decide_which_unit_to_attack(self, combat_state, coords, attacker_index):
+        # attack opponent's first ship in combat order
+
+        combat_order = combat_state[coords]
+        player_indices = [unit['player'] for unit in combat_order]
+
+        opponent_index = 1 - self.player_index
+        for combat_index, unit in enumerate(combat_order):
+            if unit['player'] == opponent_index:
+                return combat_index
---
+++ tests/level_one_tests/justin_vs_colby_tests.py
@@ -0,0 +1,43 @@
+import sys
+sys.path.append('src')
+from player import Player
+from units.unit import Unit
+from units.scout import Scout
+from units.colony_ship import ColonyShip
+from units.colony import Colony
+from game import Game
+from board import Board
+from planet import Planet
+from level_one_strategies.level_one_berserker import LevelOneBerserkerStrategy
+from level_one_strategies.level_one_dumb import LevelOneDumbStrategy
+from level_one_strategies.level_one_random import LevelOneRandomStrategy
+from level_one_strategies.level_one_george import LevelOneGeorgeStrategy
+from level_one_strategies.level_one_colby import LevelOneColbyStrategy
+from level_one_strategies.level_one_justin_berserker import LevelOneBerserkerStrategy as LevelOneJustinStrategy
+
+colby_wins = 0
+justin_wins = 0
+
+for i in range(100):
+    new_game = Game(planets=[], logging=False, die_rolls='random', invalidation=True, scouts_only = True, movement_rounds = 1, banned_phases = ['economic'], screens = False)
+    nums = [0,1]
+    if i%2 == 1:
+        nums.reverse()
+    strategy_1 = LevelOneColbyStrategy(player_num=nums[0])
+    strategy_2 = LevelOneJustinStrategy(player_num=nums[1])
+    if nums[0] == 1:
+        new_game.add_player(strategy_2, [2, 4])
+        new_game.add_player(strategy_1, [2, 0])
+    else:
+        new_game.add_player(strategy_1, [2, 0])
+        new_game.add_player(strategy_2, [2, 4])
+    new_game.initialize_game()
+    # new_game.complete_many_turns(4)
+    new_game.run_until_complete()
+    if new_game.winner_name == 'justin':
+        justin_wins += 1
+    else:
+        colby_wins += 1
+
+print('Justin Berserk won', justin_wins,'Out of 100 matches')
+print('COlby won', colby_wins,'Out of 100 matches')
