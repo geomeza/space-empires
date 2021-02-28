7b4da9b2ef4960c1a3b8680bab840dde72a9238b
geomeza <george.meza.github@gmail.com>
2021-02-28 17:52:02 +0000

    added riley strat

--- tests/level_three_tests/delayed_flanker_vs_berserker.py
+++ tests/level_three_tests/delayed_flanker_vs_berserker.py
@@ -3,20 +3,22 @@ sys.path.append('src')
 from game import Game
 from level_three_strategies.numbers_berserker import NumbersBerserkerStrategy
 from level_three_strategies.delayed_flanker import DelayedFlankerStrategy
+from level_three_strategies.riley_strat import RileyStrategy3
 import random
 import math
 
 
 berserker = NumbersBerserkerStrategy
 delayed_flanker = DelayedFlankerStrategy
+riley = RileyStrategy3
 
-strats = [berserker, delayed_flanker]
+strats = [riley, delayed_flanker]
 
 def run_game(strategy_1, strategy_2, game_num):
     strategy_1 = strategy_1(0)
     strategy_2 = strategy_2(1)
     random.seed(game_num)
-    new_game = Game(invalidation = True, logging = False, dice_rolls = 'random', level = 3, default = False)
+    new_game = Game(invalidation = True, logging = True, dice_rolls = 'random', level = 3, default = False)
     new_game.add_player(strategy_1, [3,0])
     new_game.add_player(strategy_2, [3,6])
     new_game.initialize_game()
@@ -38,7 +40,8 @@ for i in range(len(strats)):
         results = {'first': 0, 'second': 0, 'tie': 0}
         strats_to_test = [strats[i], strats[j]]
         matchup = None
-        for _ in range(500):
+        for _ in range(1):
+            print(_)
             winner = run_game(strats_to_test[0], strats_to_test[1],_+1)
             first = winner[0][0]
             second = winner[0][1]
@@ -47,15 +50,16 @@ for i in range(len(strats)):
                 matchup = winner[0]
             results[sort_counts(first, second, result)] += 1
         strats_to_test.reverse()
-        for _ in range(500):
-            winner = run_game(strats_to_test[0], strats_to_test[1],_+51)
-            first = winner[0][1]
-            second = winner[0][0]
-            result = winner[1]
-            results[sort_counts(first, second, result)] += 1
+        # for _ in range(50):
+        #     print(_+501)
+        #     winner = run_game(strats_to_test[0], strats_to_test[1],_+501)
+        #     first = winner[0][1]
+        #     second = winner[0][0]
+        #     result = winner[1]
+        #     results[sort_counts(first, second, result)] += 1
         print('-----------------------------------------')
         print(matchup[0],"VS",matchup[1])
-        print(matchup[0],'WINS:', results['first']/1000)
-        print(matchup[1],'WINS:', results['second']/1000)
-        print('TIES', results['tie']/1000)
+        print(matchup[0],'WINS:', results['first']/100)
+        print(matchup[1],'WINS:', results['second']/100)
+        print('TIES', results['tie']/100)
         print('-----------------------------------------')
